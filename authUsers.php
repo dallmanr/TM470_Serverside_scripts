c445c6274803ce52530412bce6f449cde548158c
Richard Dallman <richdallman@googlemail.com>
2017-06-28 19:21:08 +0100

    Fixed error returning JSON
    Now returns user details sucessfully if the passcode is found

--- authUsers.php
+++ authUsers.php
@@ -1,7 +1,6 @@
 <?php
  include "database.php";
  //header('Content-Type: application/json');
- //echo "<script language='javascript'>alert('authUsers.php called');</script>";
  $pw = intval($_POST['pw']);
 
  $sql = "SELECT
@@ -11,12 +10,13 @@ FROM
         INNER JOIN
     staff ON admins.staffId = staff.payeNumber
   WHERE password = $pw";
-    $result = $conn->query($sql);
-        if($result -> num_rows > 0) {
-          while ($row = $result->fetch_assoc()) {
-            $data[] = $row;
-      }
-    }
+
+  $result = $conn->query($sql);
+  	if($result -> num_rows > 0) {
+    while ($row = $result-> fetch_assoc()) {
+      $data[] = $row;
+        }
       $myJSON = json_encode($data);
       echo $myJSON;
+    }
  ?>
