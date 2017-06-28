517c8826fd97ade5b0072c0c19ca804385659c42
Richard Dallman <richdallman@googlemail.com>
2017-06-28 16:35:47 +0100

    Added server side query for authing users
    Takes a value from posted from the app and checks the admin database
    table for a password that matches and returns the user name associated
    to password.

--- authUsers.php
+++ authUsers.php
@@ -1,21 +1,22 @@
 <?php
  include "database.php";
  //header('Content-Type: application/json');
- //$pw = intval($_POST['pass']);
+ //echo "<script language='javascript'>alert('authUsers.php called');</script>";
+ $pw = intval($_POST['pw']);
+
  $sql = "SELECT
     firstName, lastName, payeNumber, password
 FROM
     admins
         INNER JOIN
     staff ON admins.staffId = staff.payeNumber
-    WHERE password = 2";
+  WHERE password = $pw";
     $result = $conn->query($sql);
-
         if($result -> num_rows > 0) {
           while ($row = $result->fetch_assoc()) {
             $data[] = $row;
       }
+    }
       $myJSON = json_encode($data);
       echo $myJSON;
-    }
  ?>
