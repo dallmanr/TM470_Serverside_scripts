520f93ef1a3f26bd4646a3ca08c27554c6fa31a9
Richard Dallman <richdallman@googlemail.com>
2017-07-06 21:27:09 +0100

    Change what is returned
    Return a JSON object with some information. Either a success message is
    stored or a fail message along with information on the reason why it
    failed.

--- addNewVan.php
+++ addNewVan.php
@@ -4,17 +4,27 @@
   $serialNumber = $_POST['serialNumber'];
   $vehicleNumber = $_POST['vehicleNumber'];
   $regNumber = $_POST['regNumber'];
-  $keysAvail = $_POST['keysAvail'];
+  $keysAvail =  $_POST['keysAvail'];
   $reasonAdded = $_POST['reasonAdded'];
   $addedBy = $_POST['addedBy'];
 
 $sql = "INSERT INTO vans (regNumber, vehicleNumber, serialNumber, numKeysAvailable, available, active, addedBy, reasonAdded)
 VALUES ('$regNumber', '$vehicleNumber', '$serialNumber', $keysAvail, 1, 1, '$addedBy', '$reasonAdded')";
 
+$data = ["status" => "",
+"data" => ""];
+
 if ($conn->query($sql) === TRUE) {
-    header("location: index.html");
+  $data["status"] = "success";
+  $data["data"] = "Van added to system";
+    //header("location: index.html");
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+  $error = $conn->error;
+  $data["status"] ="fail";
+  $data["data"]= $error;
+    //echo "Error: " . $sql . "<br>" . $conn->error;
 }
-$conn->close();
+  $myJSON = json_encode($data);
+  echo $myJSON;
+//$conn->close();
 ?>
