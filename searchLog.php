9e83893bc51c4c82de57c74b541f58b86a3e6970
Richard Dallman <richdallman@googlemail.com>
2017-07-21 20:06:47 +0100

    Fixed log query
    Now accepts a date range to search.

--- searchLog.php
+++ searchLog.php
@@ -5,6 +5,10 @@ $staffMember = intval($_POST['staffMember']);
 $vanNumber = $_POST['vanNumber'];
 $vanSerial = intval($_POST['vanSerial']);
 $vanReg = $_POST['vanReg'];
+$dateFrom = $_POST['dateFrom'];
+$dateTo = $_POST['dateTo'];
+
+
 $sql = "SELECT
     DATE(timeOut) as date,
     vanNumber,
@@ -28,28 +32,27 @@ FROM
         INNER JOIN
     staff ON dutydetails.staffMember = staff.payeNumber
 WHERE
-    staff.payeNumber = $staffMember
-    OR vans.vehicleNumber = '$vanNumber'
-    OR vans.serialNumber = $vanSerial
-    OR vans.regNumber = '$vanReg'";
+    (staff.payeNumber = $staffMember
+        OR vans.vehicleNumber = '$vanNumber'
+        OR vans.serialNumber = $vanSerial
+        OR vans.regNumber = '$vanReg')
+        AND DATE(timeOut) BETWEEN '$dateFrom' AND '$dateTo';";
 
 $result = $conn->query($sql);
 
 $data = array();
 if($result -> num_rows > 0) {
   //$data["status"] = "success";
-  //array_push($data, "success");
+  array_push($data, "success");
   //array_push($data, "data");
   while ($row = $result->fetch_assoc()) {
     $value = $row;
     array_push($data, $value);
   }
-    //header("location: index.html");
 } else {
   $error = $conn->error;
   $data["status"] = "error";
   $data["data"] = $error;
-    //echo "Error: " . $sql . "<br>" . $conn->error;
 }
   $myJSON = json_encode($data);
   echo $myJSON;
