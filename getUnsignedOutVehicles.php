a815c899d85baca62b3dde8dad33dcf2eb7c7f53
Richard Dallman <richdallman@googlemail.com>
2017-07-30 11:53:18 +0100

    Return only active and available vans
    When signing a van out, the list should only contain active and
    available vans that do not appear in the dutyDetails table with an empty
    timeIn value (they have not been signed back in so are in use by another
    member of staff).

--- getUnsignedOutVehicles.php
+++ getUnsignedOutVehicles.php
@@ -5,7 +5,7 @@ FROM vans
 AS a
 WHERE NOT EXISTS (SELECT vanNumber FROM dutyDetails AS b WHERE a.vehicleNumber = b.vanNumber
   AND DATE(timeIn) IS NULL)
-  AND active = 1
+  AND active = 1 AND available = 1
   ORDER BY vehicleNumber ASC";
 $result = $conn->query($sql);
 //echo 'Van number: ';
