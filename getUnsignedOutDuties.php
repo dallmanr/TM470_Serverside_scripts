63aa3c471ddc8305dc3b5499db0e3c48c165df60
Richard Dallman <richdallman@googlemail.com>
2017-07-31 20:38:37 +0100

    Fixed query
    Was returning only the duties that do not exist in duty details ever,
    not just that day. Any duty that currently exists in the duty details
    table was not available to sign out.

--- getUnsignedOutDuties.php
+++ getUnsignedOutDuties.php
@@ -3,7 +3,7 @@
 $sql = "SELECT dutyNumber
 FROM duty AS a
 WHERE NOT EXISTS (SELECT duty FROM dutyDetails AS b WHERE a.dutyNumber = b.duty
-  AND DATE(timeIn) IS NULL)
+  AND DATE(timeIn) IS NULL AND DATE(timeOut) = CURDATE())
   AND activeDuty = 1
   ORDER BY dutyNumber ASC";
 $result = $conn->query($sql);
