2086e49f75729b708abe00f47fb8eb0a73557c59
Richard Dallman <richdallman@googlemail.com>
2017-08-21 20:58:26 +0100

    Changed signing out query for drivers
    As I needed to add an associative table to deal with the M:N
    relationship between pdas signed out and the log, needed to update the
    query so the PDAs signed out are added to the associative table.

--- getUnsignedOutPDAs.php
+++ getUnsignedOutPDAs.php
@@ -1,26 +1,9 @@
 <?php
 include 'database.php';
 
-$sql = "SELECT
-    pdaNumber
-FROM
-    pda AS a
-WHERE
-    NOT EXISTS( SELECT
-            pdaOne
-        FROM
-            dutyDetails AS b
-        WHERE
-            b.pdaOne = a.pdaNumber
-                AND DATE(timeIn) IS NULL)
-        AND NOT EXISTS( SELECT
-            pdaTwo
-        FROM
-            dutyDetails AS c
-        WHERE
-            c.pdaTwo = a.pdaNumber
-                AND DATE(timeIn) IS NULL)
-        AND activePda = 1
+$sql = "SELECT pdaNumber FROM pda
+WHERE available = 1
+AND activePda = 1
 ORDER BY pdaNumber ASC;";
 $result = $conn->query($sql);
 
--- signAVanOut.php
+++ signAVanOut.php
@@ -11,14 +11,16 @@
   $footwear = $_POST['footwear'];
   $jacket = $_POST['jacket'];
 
+  $pdas = array ($pdaOne, $pdaTwo);
+
   $curDate = date('y/m/d');
 
-
+//First statement - Insert the values into the dutydetails table
 $sql = "INSERT INTO
 dutydetails
-  (staffMember, duty, pdaOne, pdaTwo, vanID, hiVis, footwear, postingPeg)
+  (staffMember, duty, vanID, hiVis, footwear, postingPeg)
 VALUES
-  ('$name', '$duty', $pdaOne, $pdaTwo, $vanID, '$jacket', '$footwear', '$pegs');";
+  ('$name', '$duty', $vanID, '$jacket', '$footwear', '$pegs');";
 
         $data = ["status" => ""];
 
@@ -27,10 +29,53 @@ VALUES
             //header("location: index.html");
         } else {
           $error = $conn->error;
-          $data["status"] = $error;
+          $data["status"] = "SQL1 Error - " . $error;
+          //$conn->close();
             //echo "Error: " . $sql . "<br>" . $conn->error;
         }
-          $myJSON = json_encode($data);
-          echo $myJSON;
-//$conn->close();
+
+//Second statement - Select the last row in the dutydetails table to get the dutydetails id value
+//Assign this to a variable for use in statement 3
+
+$sql2 = "SELECT MAX(dutydetails_id) as dutydetails_id FROM dutydetails";
+
+if ($conn->query($sql2) === TRUE) {
+  $data["status"] = "success";
+    //header("location: index.html");
+} else {
+  $error = $conn->error;
+  $data["status"] = "SQL2 Error - " . $error;
+  //$conn->close();
+}
+$result = mysqli_query($conn, $sql2);
+$rs = mysqli_fetch_array($result);
+$dutyid = $rs['dutydetails_id'];
+
+//Third statement - Insert into the duty_pdas table to record which PDAs a driver has signed for
+
+
+foreach ($pdas as $pda) {
+
+  $sql3 = "INSERT INTO duty_pdas (pda_id_fk, dutydetails_id_fk)
+            VALUES ($pda, $dutyid);";
+  $sql4 = "UPDATE pda SET available = 0 WHERE pdaNumber = $pda;";
+  if ($conn->query($sql3) === TRUE) {
+    $data["status"] = "success";
+      } else {
+          $error = $conn->error;
+          $data["status"] = "SQL3 Error -  " . $error;
+          //$conn->close();
+        }
+
+if ($conn->query($sql4) === TRUE) {
+  $data["status"] = "success";
+    } else {
+        $error = $conn->error;
+        $data["status"] = "SQL4 Error -  " . $error;
+        //$conn->close();
+      }
+}
+  $myJSON = json_encode($data);
+  echo $myJSON;
+  $conn->close();
 ?>
